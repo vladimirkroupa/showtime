40e18ca8f669cf6382df586a5cbdf0f07ca7b3c8
stoupa <vladimirkroupa@gmail.com>
2012-08-29 12:38:23 +0200

    Changed year type to Year.

--- src/main/java/cz/stoupa/showtimes/imports/internal/MovieYearAware.java
+++ src/main/java/cz/stoupa/showtimes/imports/internal/MovieYearAware.java
@@ -1,7 +1,10 @@
 package cz.stoupa.showtimes.imports.internal;
 
+import cz.stoupa.showtimes.domain.Year;
+import cz.stoupa.showtimes.imports.PageStructureException;
+
 public interface MovieYearAware {
 
-	int movieYear();
+	Year movieYear() throws PageStructureException;
 	
 }
